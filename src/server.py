09350c8becc166728185b849724a4c979a3fd0d5
daddysuper1 <debangabhushanboruah.com>
2025-07-12 00:10:37 +0530

    Username addition but broadcast

--- src/server.py
+++ src/server.py
@@ -9,36 +9,56 @@ server.bind((HOST, PORT))
 server.listen()
 
 clients = []
+usernames = {}  
 
-def broadcast(message, sender_socket=None):
+def broadcast(message, sender_socket=None, tag_self=None):
     for client in clients:
-        if client != sender_socket:
-            try:
-                client.send(message)
-            except:
-                remove_client(client)
-
-def handle_client(client, address):
-    while True:
         try:
-            message = client.recv(1024)
-            if message:
-                print(f"[{address}] {message.decode('utf-8')}")
-                broadcast(message, sender_socket=client)
+            if client == sender_socket:
+                if tag_self:
+                    client.send(f"You: {tag_self}".encode('utf-8'))
+            else:
+                client.send(message)
         except:
             remove_client(client)
-            break
+
+def handle_client(client, address):
+    try:
+        while True:
+            client.send(b"[SERVER] Enter a unique username: ")
+            username = client.recv(1024).decode('utf-8').strip()
+
+            if username and username not in usernames.values():
+                usernames[client] = username
+                break
+            else:
+                client.send(b"[SERVER] Username already taken or invalid. Try again.\n")
+
+        clients.append(client)
+        print(f"[+] {username} connected from {address}")
+        client.send(f"[SERVER] Welcome, {username}!".encode('utf-8'))
+
+        while True:
+            message = client.recv(1024)
+            if message:
+                decoded = message.decode('utf-8')
+                print(f"[{username}] {decoded}")
+                broadcast(f"{username}: {decoded}".encode('utf-8'), sender_socket=client, tag_self=decoded)
+    except:
+        remove_client(client)
 
 def remove_client(client):
     if client in clients:
+        username = usernames.get(client, "Unknown")
+        print(f"[-] {username} disconnected")
         clients.remove(client)
+        del usernames[client]
         client.close()
 
 def receive_connections():
     print(f"[SERVER] Listening on {HOST}:{PORT}")
     while True:
         client, address = server.accept()
-        clients.append(client)
         thread = threading.Thread(target=handle_client, args=(client, address))
         thread.start()
 
